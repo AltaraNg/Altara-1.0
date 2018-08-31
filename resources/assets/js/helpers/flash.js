50249cd537ea98150e4b4567a1acb8f9b3cea1c8
Clinsmann <ibeanuhillary@gmail.com>
2018-08-31 14:59:41 +0100

    bug fixes

--- resources/assets/js/helpers/flash.js
+++ resources/assets/js/helpers/flash.js
@@ -7,13 +7,13 @@ export default {
         this.state.success = message;
         setTimeout(() => {
             this.removeSuccess()
-        }, 5000)
+        }, 8000)
     },
     setError(message){
         this.state.error = message;
         setTimeout(() => {
             this.removeError()
-        }, 5000)
+        }, 8000)
     },
     removeSuccess(){
         this.state.success = null
