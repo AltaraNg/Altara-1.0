ea251c4b93d21406dab9e622efb45c365cf085c2
Hillary <Ibeanuhillary@gmail.com>
2019-11-13 23:31:53 +0100

    bug fix

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -19,7 +19,7 @@ class ReminderController extends Controller implements OrderConstants
         $requestObject = $this->extractRequestObject($request);
 
         $list = $requestObject['list'];
-        if (((Boolean) $request['filterWithBranch'] === true) && !isset($request['branchId']))
+        if (((Boolean)$request['filterWithBranch'] === true) && !isset($request['branchId']))
             $requestObject['branchId'] = auth('api')->user()->branch_id;
 
         $reminderList = $list == SELF::PROMISE_CALL_LIST ?
@@ -45,6 +45,15 @@ class ReminderController extends Controller implements OrderConstants
     {
         return PromiseCall::select('order_id', 'date')
             ->dateFilter('date', $requestObject)
+            ->when(isset($requestObject['filterWithBranch']),
+                function ($q1) use ($requestObject) {
+                    return $q1->whereHas("order", function ($query2) use ($requestObject) {
+                        return $query2->whereHas("storeProduct", function ($query3) use ($requestObject) {
+                            $query3->where("store_name", "=", $requestObject['branchId']);
+                        });
+                    });
+                }
+            )
             ->with(['order' => function ($query) use ($requestObject) {
                 return $query
                     ->when(isset($requestObject['list']), function ($query2) {
