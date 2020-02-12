db19acc7f729edf553ad611c63cdfcc8a7e0175a
Beaft <teebarg01@gmail.com>
2020-02-12 13:06:30 +0100

    Set Up codeception test for pipeline

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ Route::get('/data', function(){});
 Route::get('/{vue?}', function () {
     return view('welcome');
 })->where('vue', '[\/\w\.-]*');
+
+Route::any('/', function (){
+    dd('lkkkk');
+    return response().json(["message" => "working"]);
+});
