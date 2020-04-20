275f4196784aa4a18c342945f96c487da06e0c40
Beaft <teebarg01@gmail.com>
2020-04-20 15:22:10 +0100

    Implement response and exception handler

---
+++ app/Helper/ResponseCodes.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * @class: ResponseCodes
+ * @namespace: App\Helper
+ *
+ * @author: Adeniyi
+ */
+
+namespace App\Helpers;
+
+
+class ResponseCodes
+{
+    // System and Generic Errors
+    const INVALID_REQUEST = 1;
+    const SESSION_ID_REQUIRED = 2;
+    const EXCEPTION_THROWN = 4;
+    const INVALID_PARAM = 5;
+    const INCOMPLETE_PARAM = 6;
+    const ACTION_FAILED = 7;
+    const RESOURCE_NOT_FOUND = 8;
+    const FAILED_VALIDATION = 9;
+    const RESOURCE_AUTHORISATION_ERROR = 10;
+    const REQUEST_NOT_PERMITTED = 11;
+    const ROUTE_NOT_FOUND = 12;
+    const UNABLE_TO_PROCESS = 13;
+    const TEST_MODE_ONLY = 14;
+
+    // Other Errors
+    const LOGIN_FAIL = 101;
+    const USER_WITH_EMAIL_EXIST = 130;
+    const USER_NOT_EXIST = 131;
+    const PASSWORD_MISMATCH = 133;
+    const TOKEN_VERIFICATION_FAIL = 147;
+    const PROVIDER_NOT_SUPPORTED = 148;
+
+    const ACTION_SUCCESSFUL = 200;
+    const CREATED = 201;
+    const ACCEPTED = 202;
+
+    const MOVED_PERMANENTLY = 301;
+    const FOUND = 302;
+
+    const BAD_REQUEST = 400;
+    const UNAUTHENTICATED = 401;
+    const PERMISSION_DENIED = 403;
+    const NOT_FOUND = 404;
+    const METHOD_NOT_ALLOWED = 405;
+    const UNPROCESSABLE_ENTITY = 422;
+
+
+}
---
+++ app/Helper/ResponseHelper.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adeniyi
+ * Date: 06/04/2020
+ * Time: 1:27 PM
+ */
+
+namespace App\Helpers;
+
+use Illuminate\Http\Response;
+
+/**
+ * Class ResponseHelper is a helper class that helps with creating responses
+ * for the JSON API. It's benefit include providing a single-point-of-change and uniformity
+ * across all entities that return a response(such as controllers and middlewares).
+ */
+class ResponseHelper
+{
+
+    const STATUS_SUCCESS = "success";
+    const STATUS_ERROR = "error";
+
+    public static function createSuccessResponse(array $data, string $message = "", array $headers = []): Response
+    {
+        return self::createResponse(self::STATUS_SUCCESS, $data, $message, 0, 200, $headers);
+    }
+
+    public static function createResponse(
+        string $status = self::STATUS_SUCCESS,
+        array $data = [],
+        string $message = "",
+        int $code = 0,
+        int $httpResponseCode = 200,
+        array $headers = []
+    ): Response {
+        $responseData = [
+            'status' => $status,
+            'data' => $data,
+            'message' => $message
+        ];
+        if (!empty($code)) {
+            $responseData['code'] = $code;
+        }
+
+        $header    = [
+            'Access-Control-Allow-Origin'      => self::getOrigin(),
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'     => 'X-Requested-With, Content-Type, Origin, Authorization'
+        ];
+        return Response::create($responseData, $httpResponseCode, array_merge($header, $headers));
+    }
+
+    /**
+     * Returns the origin for the request. It just spits back whatever Origin
+     * request the request specifies or generates one from the $_SERVER info
+     * if no Origin header is specified for the request.
+     *
+     * @return string the origin of the request
+     */
+    public static function getOrigin(){
+        if(request()->headers->get('Origin')){
+            return request()->headers->get('Origin');
+        }
+        $server = request()->server;
+        $origin = $server->get('REQUEST_SCHEME') . "://" . $server->get('HTTP_HOST')
+                . ":" . $server->get('REMOTE_PORT');
+        return $origin;
+    }
+
+    public static function createErrorResponse(
+        $message,
+        $errorCode,
+        array $data = [],
+        $httpResponseCode = 401
+    ): Response {
+        return self::createResponse(self::STATUS_ERROR, $data, $message, $errorCode, $httpResponseCode);
+    }
+
+}
