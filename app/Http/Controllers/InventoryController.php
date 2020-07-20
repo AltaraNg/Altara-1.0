3d625f7f6c2e13cb06b992d21e2cdb24e3067ecb
Damola Oladeji <darmhoo@hotmail.com>
2020-07-20 20:38:13 +0100

    Inventory endpoint

---
+++ app/Http/Controllers/InventoryController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\InventoryRequest;
+use App\Inventory;
+use App\Repositories\InventoryRepository;
+use Illuminate\Http\Request;
+
+class InventoryController extends Controller
+{
+    //
+
+    private $inventoryRepo;
+
+    public function __construct(InventoryRepository $inventoryRepository)
+    {
+        $this->inventoryRepo = $inventoryRepository;
+    }
+
+    public function index(InventoryController $filter)
+    {
+        $inventories = $this->inventoryRepo->all();
+
+        return $this->sendSuccess($inventories->toArray(), 'Inventory retrieved successfully');
+    }
+
+    public function show(Inventory $inventory)
+    {
+        return $this->sendSuccess($inventory->toArray(), 'Inventory retrieved successfully');
+    }
+
+    public function store(InventoryRequest $request)
+    {
+        $inv = $this->inventoryRepo->store($request->validated());
+
+        return $this->sendSuccess($inv->toArray(), 'Inventory Successfully Created');
+    }
+
+    public function update(Inventory $inventory, InventoryRequest $request)
+    {
+        $inventory = $this->inventoryRepo->update($inventory, $request->validated());
+
+        return $this->sendSuccess($inventory->toArray(), 'Inventory updated successfully');
+    }
+
+    public function destroy(Inventory $inventory)
+    {
+        $inventory->delete();
+
+        return $this->sendSuccess([],'Inventory deleted successfully');
+    }
+}
---
+++ database/migrations/new/2020_07_16_225416_create_inventories_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateInventoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id')->index()->nullable();
+            $table->unsignedInteger('receiver_id')->index()->nullable();
+            $table->unsignedInteger('branch_id')->index()->nullable();
+            $table->unsignedInteger('supplier_id')->index()->nullable();
+            $table->string('inventory_sku')->unique();
+            $table->double('price');
+            $table->string('received_date');
+            $table->string('sold_date');
+            $table->boolean('is_active')->default(1);
+            $table->timestamps();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('receiver_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('branch_id')->references('id')->on('branches')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventories');
+    }
+}
