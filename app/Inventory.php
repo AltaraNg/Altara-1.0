027f0662867d35bd9c6be9e8073e087fb920a43d
Beaft <teebarg01@gmail.com>
2020-07-23 14:37:34 +0100

    Search feature

--- app/Inventory.php
+++ app/Inventory.php
@@ -2,11 +2,12 @@
 
 namespace App;
 
+use App\Http\Filters\Filterable;
 use Illuminate\Database\Eloquent\Model;
 
 class Inventory extends Model
 {
-    //
+    use Filterable;
     protected $guarded = [];
     /**
      * Validation rules
@@ -20,12 +21,13 @@ class Inventory extends Model
         return [
             'inventory_sku' => 'required|unique:inventories,inventory_sku|string',
             'product_id' => 'required|exists:products,id',
+            'product_name' => 'required|exists:products,name',
             'receiver_id' => 'required|exists:users,id',
             'branch_id' => 'required|exists:branches,id',
             'supplier_id' => 'required|exists:suppliers,id',
             'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
-            'received_date' => 'required',
-            'sold_date' => 'required',
+            'received_date' => 'required|date',
+            'sold_date' => 'required|date',
             'is_active' => 'sometimes|required|boolean'
         ];
     }
@@ -42,6 +44,7 @@ class Inventory extends Model
         return [
             'is_active' => 'sometimes|required|boolean',
             'product_id' => 'sometimes|required|exists:products,id',
+            'product_name' => 'sometimes|required|exists:products,name',
             'receiver_id' => 'sometimes|required|exists:users,id',
             'branch_id' => 'sometimes|required|exists:branches,id',
             'supplier_id' => 'sometimes|required|exists:suppliers,id',
