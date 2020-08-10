4cd6c913d7c963e2cfeefd264661d720081e1623
Damola Oladeji <darmhoo@hotmail.com>
2020-08-11 00:17:56 +0100

    Search Inventory

--- app/Http/Filters/ProductFilter.php
+++ app/Http/Filters/ProductFilter.php
@@ -6,4 +6,15 @@ use Illuminate\Database\Eloquent\Builder;
 
 class ProductFilter extends BaseFilter
 {
+    public function brand(int $brand_id){
+        $this->builder->where('brand_id', 'like', '%' . $brand_id .'%');
+    }
+
+    public function category(int $category_id){
+        $this->builder->where('category_id', 'like', '%' . $category_id .'%');
+    }
+
+    public function productType(int $product_type_id){
+        $this->builder->where('product_type_id', 'like', '%' . $product_type_id .'%');
+    }
 }
