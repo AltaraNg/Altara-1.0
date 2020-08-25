3b465fe98babc3647aa36f44e9dbe45ad92660a9
Beaft <teebarg01@gmail.com>
2020-08-25 10:47:05 +0100

    Merge branch 'staging' into Ammortization
    # Conflicts:
    #	app/Http/Filters/ProductFilter.php

--- app/Http/Filters/ProductFilter.php
+++ app/Http/Filters/ProductFilter.php
@@ -6,16 +6,12 @@ use Illuminate\Database\Eloquent\Builder;
 
 class ProductFilter extends BaseFilter
 {
-    public function brand(int $brand_id){
-        $this->builder->where('brand_id', 'like', '%' . $brand_id .'%');
-    }
-
     public function category(int $category_id){
-        $this->builder->where('category_id', 'like', '%' . $category_id .'%');
+        $this->builder->where('category_id', $category_id);
     }
 
     public function productType(int $product_type_id){
-        $this->builder->where('product_type_id', 'like', '%' . $product_type_id .'%');
+        $this->builder->where('product_type_id', $product_type_id);
     }
     /**
      * @param int $id
