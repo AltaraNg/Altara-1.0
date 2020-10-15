b742dbc2195c60209f228b7899d30378e202742b
jhayX <lordsylharx@gmail.com>
2020-10-15 14:50:37 +0100

    fixed modal

--- resources/assets/js/views/DVA/messaging/message.vue
+++ resources/assets/js/views/DVA/messaging/message.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
     <transition name="fade">
         <div class="pt-md-3 pt-2" id="employeeRegister">
             <div class="card">
@@ -60,7 +61,49 @@
                 </div>
             </div>
         </div>
+
+
     </transition>
+    <div class="modal fade repayment" id="sms_modal">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header py-2">
+                            <h6 class="modal-title py-1">SMS Response</h6>
+                            <a aria-label="Close" class="close py-1" data-dismiss="modal">
+                                <span aria-hidden="true" class="modal-close text-danger"><i class="fas fa-times"></i></span>
+                            </a>
+                        </div>
+                        <div class="modal-body" v-if="showModalContent1">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-striped">
+                                    <thead>
+                                    <tr>
+                                        <th>Phone</th>
+                                        <th>Response Type</th>
+                                        <th>Description</th>
+                                        
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr v-for="type in dataMessages">
+                                        
+                                        <td>{{type.to}}</td>
+                                        <td>{{type.status.groupName}}</td>
+                                        <td>{{type.status.description}}</td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                            
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <a class="text-link mt-3 w-100" data-dismiss="modal" href="javascript:"
+                            style="text-align: right" >close dialogue</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+</div>
 </template>
 <script>
 import { Message } from "../../../utilities/sms";
@@ -71,7 +114,10 @@ export default {
         return {
             contacts: "",
             message: "",
-            form: {}
+            form: {},
+            dataMessages:[],
+            showModalContent1:true,
+
         };
     },
     methods: {
@@ -84,20 +130,17 @@ export default {
                             
                             r.status === 200;
                             const success = [1,3];
-                            const data = r.data.messages[0].status;
-
-                            const respose = success.includes(data.groupId) ? 'success' : 'error';
-                            this.$swal({
-                                icon: respose,
-                                title: `${data.groupName}`,
-                                text: `${data.description}`
-                            }) && this.done();
+                            const data = r.data.messages;
+                            console.log('dataMessages',data);
+                            this.dataMessages=data;
+                             this.done();
                         });
                     } else this.$networkErr();
                 } else this.$networkErr("form");
             });
         },
         done() {
+            $(`#sms_modal`).modal("toggle");
             this.$scrollToTop();
             this.$LIPS(false);
             // Flash.setSuccess("done!");
