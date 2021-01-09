563f7d3cb6614a3a7a160c486033ec88570ce0b2
Beaft <teebarg01@gmail.com>
2021-01-09 20:57:02 +0100

    Update migration

--- database/migrations/new/2020_06_01_192201_create_custom_repayment_dates_table.php
+++ database/migrations/new/2020_06_01_192201_create_custom_repayment_dates_table.php
@@ -16,7 +16,7 @@ class CreateCustomRepaymentDatesTable extends Migration
         Schema::create('custom_repayment_dates', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('new_order_id')->index();
-            $table->foreign('new_order_id')->references('id')->on('new_orders')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('new_order_id')->references('id')->on('new_orders');
             $table->integer('custom_date')->default(1);
             $table->timestamps();
         });
