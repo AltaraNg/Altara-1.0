b7dd47c9fadfa81ba64374eae364ea80709d9294
adeniyiaderounmu <teebarg01@gmail.com>
2021-02-28 09:37:02 +0100

    import update

--- app/Imports/InventoriesImport.php
+++ app/Imports/InventoriesImport.php
@@ -12,10 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 use Maatwebsite\Excel\Concerns\ToCollection;
-use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Maatwebsite\Excel\Concerns\WithChunkReading;
-use Maatwebsite\Excel\Concerns\WithBatchInserts;
 
 class InventoriesImport implements ToCollection, WithHeadingRow
 {
@@ -28,11 +25,8 @@ class InventoriesImport implements ToCollection, WithHeadingRow
         $branches = Branch::all();
         $supplier_id = Supplier::first()->id;
         $inventory_id = InventoryStatus::where('status', InventoryStatus::AVAILABLE)->first()->id;
-        $t = 0;
 
         foreach ($collection as $row) {
-            $t++;
-            print_r($t);
             foreach ($branches as $branch) {
                 $branchName = Str::snake(str_replace('-', " ", $branch->name));
                 if (array_key_exists($branchName, $row->toArray()) && $row[$branchName] > 0){
@@ -48,26 +42,6 @@ class InventoriesImport implements ToCollection, WithHeadingRow
                     $target = $row[$branchName] - $invDic->quantity;
 
                     $all = [];
-                    // for ($i = 0; $i < $target; $i++) {
-                    //     $inventory = new Inventory();
-                    //     $inventory['price'] = $product->retail_price;
-                    //     $inventory['product_id'] = $product->id;
-                    //     $inventory['product_name'] = $product->name;
-                    //     $inventory['receiver_id'] = auth()->user()->id;
-                    //     $inventory['supplier_id'] = $supplier_id;
-                    //     $inventory['sku'] = '';
-                    //     $inventory['received_date'] = Carbon::today()->toDateString();
-                    //     $inventory['inventory_status_id'] = $inventory_id;
-                    //     $inventory['branch_id'] = $branch->id;
-                    //     $data = $inventory->toArray();
-                    //     unset($data['inventory_status']);
-                    //     unset($data['transfers']);
-                    //     unset($data['sold_date']);
-                    //     unset($data['created_at']);
-                    //     unset($data['updated_at']);
-                    //     unset($data['id']);
-                    //     $all[] = $data;
-                    // }
 
                     for ($i = 0; $i < $target; $i++) {
                         $data = [
@@ -85,19 +59,6 @@ class InventoriesImport implements ToCollection, WithHeadingRow
                         $all[] = $data;
                     }
 
-                    // $array = [
-                    //     'price' => $product->retail_price,
-                    //     'product_id' => $product->id,
-                    //     'product_name' => $product->name,
-                    //     'receiver_id' => auth()->user()->id,
-                    //     'supplier_id' => $supplier_id,
-                    //     'inventory_sku' => Inventory::getInventorySku(),
-                    //     'received_date' => Carbon::today()->toDateString(),
-                    //     'inventory_status_id' => $inventory_id,
-                    //     'branch_id' => $branch->id,
-                    // ];
-
-                    // $all = array_fill(0, $target, $array);
                     if (count($all)) {
                         Inventory::insert($all);
 
