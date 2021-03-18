1507cdaf80595415bf699244a345f49403869a6c
Damola Oladeji <darmhoo@hotmail.com>
2021-03-18 19:21:08 +0100

    fixed active users

--- app/Role.php
+++ app/Role.php
@@ -12,7 +12,7 @@ class Role extends Model
     //a role can be assigned to many user/employee : role->users
     public function users()
     {
-        return $this->hasMany(User::class, 'role_id', 'id');
+        return $this->hasMany(User::class, 'role_id', 'id')->where('portal_access', 1);
     }
 
     public function salesCategories()
