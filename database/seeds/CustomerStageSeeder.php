333f9420f84ff7abc842ef5976cce5c3d5fecbbe
Damola Oladeji <darmhoo@hotmail.com>
2021-03-29 13:13:53 +0100

    Revert "added seeder for customer stage"
    This reverts commit 30a374ad7e73285b58509d53e99edf6512c83002.

--- database/seeds/CustomerStageSeeder.php
+++ database/seeds/CustomerStageSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace Database\Seeders;
 
 use App\CustomerStage;
 use Illuminate\Database\Seeder;
@@ -14,41 +15,13 @@ class CustomerStageSeeder extends Seeder
      */
     public function run()
     {
-        CustomerStage::create([
-            "name" => "Registered On Portal",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Paid Affidavit",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "KYC Documents Submitted",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Passed Verification",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Failed Verification",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Ready to pay downpayment (Product Available)",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Ready to pay downpayment (Product Unavailable)",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Paid Downpayment + Product Picked Up",
-            "is_active" => true
-        ]);
-        CustomerStage::create([
-            "name" => "Paid Downpayment + Product Not Picked Up",
-            "is_active" => true
-        ]);
+        $status = ["name" => CustomerStage::CONTACTED];
+        DB::table("customer_stages")->updateOrInsert($status);
+        $status = ["name" => CustomerStage::REGISTERED];
+        DB::table("customer_stages")->updateOrInsert($status);
+        $status = ["name" => CustomerStage::AFFIDAVIT];
+        DB::table("customer_stages")->updateOrInsert($status);
+        $status = ["name" => CustomerStage::PURCHASED];
+        DB::table("customer_stages")->updateOrInsert($status);
     }
 }
