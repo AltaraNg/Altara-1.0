113b61ddbe372063e6453d96302f7e3d2d34bcb6
harmlessprince <realolamilekan@gmail>
2021-08-06 15:01:25 +0100

    implimented stat report method

--- app/Http/Controllers/NewOrderController.php
+++ app/Http/Controllers/NewOrderController.php
@@ -75,4 +75,20 @@ class NewOrderController extends Controller
         $result = $this->newOrderRepository->repossess($new_order);
         return $this->sendSuccess($result, 'Order repossessed successfully');
     }
+    public function report(NewOrderFilter $filter)
+    {
+        $newOrdersQuery = $this->newOrderRepository->query($filter)->latest();
+        $newOrders = clone $newOrdersQuery->get();
+        $orders =  $newOrders->groupBy('branch.name');
+        $additional =  $orders->map(function ($item, $key) {
+            return [
+                'branch_name' => $item[0]->branch->name,
+                'avg_price_of_prod_per_show_room' => number_format($item->avg('product_price'), 2),
+                'total_potential_revenue_sold_per_showroom' => number_format($item->avg('product_price') * count($item), 2),
+                'number_of_sales' => count($item),
+            ];
+        });
+        $additional = $additional->put('total_no_sales', count($newOrders));
+        return $this->sendSuccess([$newOrdersQuery->paginate((int)request('limit', 10)), "meta" => $additional], 'Orders retrieved successfully');
+    }
 }
