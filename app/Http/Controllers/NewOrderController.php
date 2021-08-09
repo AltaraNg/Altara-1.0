a035a993b9ff3d9a0dd6a3dd660ed3be977c05f7
harmlessprince <realolamilekan@gmail>
2021-08-09 21:08:09 +0100

    stat for altara pay and altara cash is now been computed

--- app/Http/Controllers/NewOrderController.php
+++ app/Http/Controllers/NewOrderController.php
@@ -78,9 +78,10 @@ class NewOrderController extends Controller
     public function report(NewOrderFilter $filter)
     {
         $newOrdersQuery = $this->newOrderRepository->query($filter)->latest();
-        $newOrders = clone $newOrdersQuery->get();
-        $orders =  $newOrders->groupBy('branch.name');
-        $additional =  $orders->map(function ($item, $key) {
+        $newOrdersForComputation = clone $newOrdersQuery;
+        $newOrdersToBeGrouped = clone $newOrdersQuery->get();
+        $ordersGroupedByBranch =  $newOrdersToBeGrouped->groupBy('branch.name');
+        $additional =  $ordersGroupedByBranch->map(function ($item, $key) {
             return [
                 'branch_name' => $item[0]->branch->name,
                 'avg_price_of_prod_per_show_room' => number_format($item->avg('product_price'), 2),
@@ -88,7 +89,20 @@ class NewOrderController extends Controller
                 'number_of_sales' => count($item),
             ];
         });
-        $additional = $additional->put('total_no_sales', count($newOrders));
+        $totalAltaraPay = (clone $newOrdersForComputation)->whereHas('businessType', function ($query) {
+            $query->where('name', 'like', '%Altara Pay%');
+        })->count();
+        $totalAltaraCash = (clone $newOrdersForComputation)->whereHas('businessType', function ($query) {
+            $query->where('name', 'like', '%Altara Credit%');
+        })->count();
+        $totalSales = count($newOrdersToBeGrouped);
+        $additional = $additional->put('total_no_sales', $totalSales);
+        $additional = $additional->put('altaraPayVersusAltaraCash', [
+            'no_of_sales_altara_cash' => $totalAltaraCash,
+            'no_of_sales_altara_pay' => $totalAltaraPay,
+            'percentage_of_sales_altara_pay' => number_format(($totalAltaraPay / $totalSales) * 100, 2),
+            'percentage_of_sales_altara_cash' => number_format(($totalAltaraCash / $totalSales) * 100, 2)
+        ]);
         return $this->sendSuccess([$newOrdersQuery->paginate((int)request('limit', 10)), "meta" => $additional], 'Orders retrieved successfully');
     }
 }
