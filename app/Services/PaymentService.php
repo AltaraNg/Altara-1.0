9d9454121721d51bce000cdb94bac4271a718488
Damola Oladeji <darmhoo@hotmail.com>
2021-08-09 07:34:20 +0100

    done

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -14,15 +14,17 @@ class PaymentService
 {
     public static function logPayment($data, $model)
     {
+
+
         $trans = PaymentReconcile::firstOrCreate(
-            ['branch_id' => auth()->user()->branch_id, 'payment_method_id' =>$data['payment_method_id'], 'date' => Carbon::today()],
+            ['branch_id' => $model->branch_id, 'payment_method_id' =>$data['payment_method_id'], 'date' => Carbon::today()],
             ['reconcile_number' => Helper::generateTansactionNumber('RE')]
         );
 
         $payment = $model->payments()->create(array_merge($data, [
             'payment_number' => Helper::generateTansactionNumber('PM'),
-            'user_id' => auth()->user()->id,
-            'branch_id' => auth()->user()->branch_id,
+            'user_id' => $model->owner_id,
+            'branch_id' => $model->branch_id,
             'payment_reconcile_id' => $trans->id,
         ]));
 
