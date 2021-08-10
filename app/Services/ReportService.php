2c4d02461e3485e480145f2270ed2576a9c58539
harmlessprince <realolamilekan@gmail>
2021-08-10 13:48:23 +0100

    refactored ReportService Class

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -4,12 +4,27 @@ namespace App\Services;
 
 class ReportService
 {
-    static function generateMetaData($newOrdersQuery)
+    public static function generateMetaData($newOrdersQuery)
     {
         $newOrdersForComputation = clone $newOrdersQuery;
         $newOrdersToBeGrouped = clone $newOrdersQuery->get();
+        $additional = self::groupOrderByBranchName($newOrdersToBeGrouped);
+        $totalAltaraPay = self::getNoOfAltaraPayProduct(clone $newOrdersForComputation);
+        $totalAltaraCash = self::getNoOfAltaraCashProduct(clone $newOrdersForComputation);
+        $totalSales = count($newOrdersToBeGrouped);
+        $totalRevenue = $newOrdersToBeGrouped->avg('product_price') * $totalSales;
+        $revenuePerSale = $totalRevenue / $totalSales;
+        $additional = $additional->put('altaraPayVersusAltaraCash', self::getComparismOfAltaraPayVsAltaraCash($totalAltaraCash, $totalAltaraPay, $totalSales));
+        $additional = $additional->put('total_no_sales', $totalSales);
+        $additional = $additional->put('total_revenue', number_format($totalRevenue, 2));
+        $additional = $additional->put('revenue_per_sale', number_format($revenuePerSale, 2));
+        return $additional;
+    }
+
+    private static function groupOrderByBranchName($newOrdersToBeGrouped)
+    {
         $ordersGroupedByBranch =  $newOrdersToBeGrouped->groupBy('branch.name');
-        $additional =  $ordersGroupedByBranch->map(function ($item, $key) {
+        return  $ordersGroupedByBranch->map(function ($item, $key) {
             return [
                 'branch_name' => $item[0]->branch->name,
                 'avg_price_of_prod_per_show_room' => number_format($item->avg('product_price'), 2),
@@ -17,24 +32,30 @@ class ReportService
                 'number_of_sales' => count($item),
             ];
         });
-        $totalAltaraPay = (clone $newOrdersForComputation)->whereHas('businessType', function ($query) {
+    }
+
+    private static function getNoOfAltaraPayProduct($newOrdersForComputation)
+    {
+        return $newOrdersForComputation->whereHas('businessType', function ($query) {
             $query->where('name', 'like', '%Altara Pay%');
         })->count();
-        $totalAltaraCash = (clone $newOrdersForComputation)->whereHas('businessType', function ($query) {
+    }
+
+    private static function getNoOfAltaraCashProduct($newOrdersForComputation)
+    {
+        return $newOrdersForComputation->whereHas('businessType', function ($query) {
             $query->where('name', 'like', '%Altara Credit%');
         })->count();
-        $totalSales = count($newOrdersToBeGrouped);
-        $totalRevenue = $newOrdersToBeGrouped->avg('product_price') * $totalSales;
-        $revenuePerSale = $totalRevenue / $totalSales;
-        $additional = $additional->put('altaraPayVersusAltaraCash', [
+    }
+
+
+    private static function getComparismOfAltaraPayVsAltaraCash($totalAltaraCash, $totalAltaraPay, $totalSales)
+    {
+        return [
             'no_of_sales_altara_cash' => $totalAltaraCash,
             'no_of_sales_altara_pay' => $totalAltaraPay,
             'percentage_of_sales_altara_pay' => number_format(($totalAltaraPay / $totalSales) * 100, 2),
             'percentage_of_sales_altara_cash' => number_format(($totalAltaraCash / $totalSales) * 100, 2)
-        ]);
-        $additional = $additional->put('total_no_sales', $totalSales);
-        $additional = $additional->put('total_revenue', number_format($totalRevenue, 2));
-        $additional = $additional->put('revenue_per_sale', number_format($revenuePerSale, 2));
-        return $additional;
+        ];
     }
 }
