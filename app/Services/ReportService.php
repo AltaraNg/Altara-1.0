6a7f034e1fb5279a90a2b82939daffe0f06182c5
harmlessprince <realolamilekan@gmail>
2021-08-11 22:33:26 +0100

    added stat for showing number of altarapay and altaracash product per branch

--- app/Services/ReportService.php
+++ app/Services/ReportService.php
@@ -7,10 +7,10 @@ class ReportService
     public static function generateMetaData($newOrdersQuery)
     {
         $newOrdersForComputation = clone $newOrdersQuery;
-        $newOrdersToBeGrouped = clone $newOrdersQuery->get();
-        $totalSales = count($newOrdersToBeGrouped);
-        $totalRevenue = $newOrdersToBeGrouped->avg('product_price') * $totalSales;
-        $additional = self::groupOrderByBranchName($newOrdersToBeGrouped, $totalRevenue);
+        // $newOrdersToBeGrouped = clone $newOrdersQuery->get();
+        $totalSales = $newOrdersQuery->count();
+        $totalRevenue = $newOrdersQuery->avg('product_price') * $totalSales;
+        $additional = self::groupOrderByBranchName(clone $newOrdersQuery, $totalRevenue);
         $totalAltaraPay = self::getNoOfAltaraPayProduct(clone $newOrdersForComputation);
         $totalAltaraCash = self::getNoOfAltaraCashProduct(clone $newOrdersForComputation);
         //to prevent division by zero error
@@ -24,16 +24,22 @@ class ReportService
 
     private static function groupOrderByBranchName($newOrdersToBeGrouped, $totalRevenue)
     {
-        $ordersGroupedByBranch =  $newOrdersToBeGrouped->groupBy('branch.name');
-        return  $ordersGroupedByBranch->map(function ($item, $key) use ($totalRevenue) {
+        $dummy = clone $newOrdersToBeGrouped;
+        $ordersGroupedByBranch =   $newOrdersToBeGrouped->get()->groupBy('branch.name');
+        return  $ordersGroupedByBranch->map(function ($item, $key) use ($totalRevenue, $dummy) {
             $totalPotentialRevenuePerShowroom = $item->avg('product_price') * count($item);
             $percentageOfTotalRevenue = $totalPotentialRevenuePerShowroom / $totalRevenue * 100;
+            $countPay = self::getNoOfAltaraPayProductPerBranch(clone $dummy, $item[0]->branch->id);
+            $countCash = self::getNoOfAltaraCashProductPerBranch(clone $dummy, $item[0]->branch->id);
             return [
+                'branch_id' => $item[0]->branch->id,
                 'branch_name' => $item[0]->branch->name,
-                'avg_price_of_prod_per_show_room' => number_format($item->avg('product_price'), 2),
-                'total_potential_revenue_sold_per_showroom' => number_format($totalPotentialRevenuePerShowroom , 2),
-                'number_of_sales' => count($item), 
+                'avg_price_of_prod_per_showroom' => number_format($item->avg('product_price'), 2),
+                'total_potential_revenue_sold_per_showroom' => number_format($totalPotentialRevenuePerShowroom, 2),
+                'number_of_sales' => count($item),
                 'percentage_of_total_revenues' => number_format($percentageOfTotalRevenue, 3),
+                'no_of_altara_pay' => $countPay,
+                'no_of_altara_cash' => $countCash,
             ];
         });
     }
@@ -42,6 +48,19 @@ class ReportService
     {
         return $newOrdersForComputation->whereHas('businessType', function ($query) {
             $query->where('name', 'like', '%Altara Pay%');
+        })->count();
+    }
+
+    private static function getNoOfAltaraPayProductPerBranch($newOrdersForComputation, $branch_id)
+    {
+        return $newOrdersForComputation->where('branch_id', $branch_id)->whereHas('businessType', function ($query) {
+            $query->where('name', 'like', '%Altara Pay%');
+        })->count();
+    }
+    private static function getNoOfAltaraCashProductPerBranch($newOrdersForComputation, $branch_id)
+    {
+        return $newOrdersForComputation->where('branch_id', $branch_id)->whereHas('businessType', function ($query) {
+            $query->where('name', 'like', '%Altara Credit%');
         })->count();
     }
 
@@ -60,8 +79,8 @@ class ReportService
         return [
             'no_of_sales_altara_cash' => $totalAltaraCash,
             'no_of_sales_altara_pay' => $totalAltaraPay,
-            'percentage_of_sales_altara_pay' => number_format(($totalAltaraPay / $totalSales) * 100, 2) ,
-            'percentage_of_sales_altara_cash' => number_format(($totalAltaraCash / $totalSales) * 100, 2) 
+            'percentage_of_sales_altara_pay' => number_format(($totalAltaraPay / $totalSales) * 100, 2),
+            'percentage_of_sales_altara_cash' => number_format(($totalAltaraCash / $totalSales) * 100, 2)
         ];
     }
 }
