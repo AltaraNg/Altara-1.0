087140b5ff14bbf20e484b15107e53f9dba13a56
harmlessprince <realolamilekan@gmail>
2021-08-18 14:12:31 +0100

    added order_types table and foriegn key constraint on new_orders table

---
+++ database/migrations/new/2021_08_18_130530_update_new_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateNewOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('new_orders', function (Blueprint $table) {
+            $table->unsignedInteger('order_type_id')->nullable();
+            $table->foreign('order_type_id')->references('id')->on('order_types');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('new_orders', function (Blueprint $table) {
+            //
+            $table->dropColumn('order_type_id');
+        });
+    }
+}
