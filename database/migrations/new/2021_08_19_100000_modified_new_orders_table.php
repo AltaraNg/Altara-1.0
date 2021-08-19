d6abdcb7b00ae9caa173a76ba9f5c15e02223a67
realolamilekan@gmail.com <realolamilekan@gmail.com>
2021-08-19 11:07:49 +0100

    added migration modify new orders table

--- database/migrations/new/2021_08_19_100000_modified_new_orders_table.php
+++ database/migrations/new/2021_08_19_100000_modified_new_orders_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddOrderTypeToNewOrdersTable extends Migration
+class ModifiedNewOrdersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,8 +14,14 @@ class AddOrderTypeToNewOrdersTable extends Migration
     public function up()
     {
         Schema::table('new_orders', function (Blueprint $table) {
+            //
+            $table->unsignedInteger('down_payment_rate_id')->nullable();
+            $table->unsignedInteger('payment_gateway_id')->nullable();
             $table->unsignedInteger('order_type_id')->nullable();
+            //foreign keys
+            $table->foreign('down_payment_rate_id')->references('id')->on('down_payment_rates');
             $table->foreign('order_type_id')->references('id')->on('order_types');
+            $table->foreign('payment_gateway_id')->references('id')->on('payment_gateways');
         });
     }
 
@@ -27,8 +33,9 @@ class AddOrderTypeToNewOrdersTable extends Migration
     public function down()
     {
         Schema::table('new_orders', function (Blueprint $table) {
-            //
-            $table->dropColumn('order_type_id');
+            $table->dropColumn('down_payment_rate_id');
+            $table->dropColumn('order_type_id'); 
+            $table->dropColumn('payment_gateway_id');
         });
     }
 }
