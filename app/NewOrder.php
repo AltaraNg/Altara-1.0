1e069f84fafd10f58bf63e16f34471c709441ab6
harmlessprince <realolamilekan@gmail>
2021-08-23 13:30:55 +0100

    orders can now be updated

--- app/NewOrder.php
+++ app/NewOrder.php
@@ -64,6 +64,9 @@ class NewOrder extends Model
             'down_payment' => ['sometimes', 'required', new Money],
             'product_price' => ['sometimes', 'required', new Money],
             'order_date' => 'sometimes|required|date',
+            'down_payment_rate_id' => 'sometimes|exists:down_payment_rates,id',
+            'order_type_id' => 'sometimes|exists:order_types,id',
+            'payment_gateway_id' => 'sometimes|exists:payment_gateways,id',
         ];
     }
 
