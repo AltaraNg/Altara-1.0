e4ac273bec691cc892e3c89bf6dbbe650edf79c2
harmlessprince <realolamilekan@gmail>
2021-08-25 12:49:04 +0100

    command for updating downpayment rate id on new_orders table now working

--- app/Console/Commands/PopulateDownPaymentCommand.php
+++ app/Console/Commands/PopulateDownPaymentCommand.php
@@ -6,11 +6,14 @@ use App\AltaraPayDdData;
 use App\DownPaymentRate;
 use App\Log;
 use App\NewOrder;
+use App\Repositories\NewOrderRepository;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Log as FacadesLog;
 
 class PopulateDownPaymentCommand extends Command
 {
+    private $downPaymentRates;
+    private $newOrderRepo;
     /**
      * The name and signature of the console command.
      *
@@ -23,16 +26,18 @@ class PopulateDownPaymentCommand extends Command
      *
      * @var string
      */
-    protected $description = 'Populates NewOrder Table with Downpayment ID';
+    protected $description = 'Populates NewOrder Table with Down Payment ID';
 
     /**
      * Create a new command instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(NewOrderRepository $newOrderRepository)
     {
         parent::__construct();
+        $this->downPaymentRates = DownPaymentRate::all();
+        $this->newOrderRepo = $newOrderRepository;
     }
 
     /**
@@ -42,12 +47,60 @@ class PopulateDownPaymentCommand extends Command
      */
     public function handle()
     {
-        $orders =  NewOrder::whereNotNull(['down_payment', 'product_price'])->get()->take(20);
-        $downpaymentRates = DownPaymentRate::all();
-        $orders->each(function ($order) use ($downpaymentRates) {
-            $percent = ceil(($order->down_payment / $order->product_price) * 100);
-            $downpaymentRate = $downpaymentRates->firstWhere('percent', '=', $percent);
-            $this->info($downpaymentRate->id);
-        });
+        $orders =  NewOrder::whereNotNull('down_payment')
+            ->whereNotNull('down_payment')
+            ->whereNull('down_payment_rate_id')
+            ->get();
+        $this->info('Found ' . $orders->count() . ' new orders');
+        $count = 0;
+        foreach ($orders as $order) {
+            $percent = ($order->down_payment / $order->product_price) * 100;
+            if ($percent < 5) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(0));
+                $count++;
+                continue;
+            }
+
+            if ($percent >= 5 && $percent <= 14) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(10));
+                $count++;
+                continue;
+            }
+            if ($percent >= 15 && $percent <= 29) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(20));
+                $count++;
+                continue;
+            }
+            if ($percent >= 30 && $percent <= 49) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(40));
+                $count++;
+                continue;
+            }
+            if ($percent >= 50 && $percent <= 69) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(60));
+                $count++;
+                continue;
+            }
+            if ($percent >= 70) {
+                $this->updateNewOrderDownPaymentOrderID($order, $this->getDownPaymentID(80));
+                $count++;
+                continue;
+            }
+        }
+        $this->info($count . ' New Orders Updated');
+    }
+
+    private  function getDownPaymentID($downPaymentRatePercent)
+    {
+        $downPaymentRate = $this->downPaymentRates->firstWhere('percent', '=', $downPaymentRatePercent);
+        return $downPaymentRate->id;
+    }
+
+    private function updateNewOrderDownPaymentOrderID($order, $downPaymentRateID)
+    {
+        $this->newOrderRepo->update($order, [
+            'down_payment_rate_id' =>  $downPaymentRateID,
+        ]);
+        $this->info('Updated Made');
     }
 }
