28578acec8bc8afa66578875adf1ac1df7990381
realolamilekan@gmail.com <realolamilekan@gmail.com>
2021-08-26 13:58:14 +0100

    All branches when filtering are now been returned

--- routes/api.php
+++ routes/api.php
@@ -122,6 +122,7 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/order/reports/export', 'ReportController@getNewOrdersReport');
 
 });
+Route::get('/order/reports', 'NewOrderController@report');
 Route::post('/ammo', 'UserController@test');
 Route::post('/credit-check', 'CreditCheckController@check');
 //Route::apiResource('amortization', 'AmortizationController');
