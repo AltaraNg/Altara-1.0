7d4f20066382d567adf86f4ffc371c64fdb25002
Adeniyi <teebarg01@gmail.com>
2021-09-05 20:12:58 +0100

    refactor direct debit

--- app/OrderType.php
+++ app/OrderType.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class OrderType extends Model
 {
     protected $table = "order_types";
+    const ALTARA_CREDIT = 'Altara Credit';
+    const ALTARA_PAY = 'Altara Pay';
 }
--- app/PaymentGateway.php
+++ app/PaymentGateway.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PaymentGateway extends Model
 {
     protected $table = "payment_gateways";
+    const PAYSTACK = 'Paystack';
+    const REMITTA = 'Remitta';
 
 }
