9190a1b9b39931fef58c800fa6124fd61fc87f45
Adeniyi <teebarg01@gmail.com>
2021-10-18 12:37:22 +0100

    update business type

--- app/BusinessType.php
+++ app/BusinessType.php
@@ -10,8 +10,8 @@ class BusinessType extends Model
         const ALTARA_CREDIT_PRODUCT = 'Altara Credit Products';
         const ALTARA_PAY_PRODUCT = 'Altara Pay Products';
         const ALTARA_CREDIT_CASH_LOAN = 'Altara Credit Cash Loan';
-        const ALTARA_PAY_CASH_LOAN = 'Altara Pay Cash Loan';
-        const ALTARA_PAY_CASH_LOAN_PRODUCT = 'Altara Pay Cash Loan(Product)';
+        const ALTARA_PAY_CASH_LOAN = 'Altara Pay Renewal Cash Loan';
+        const ALTARA_PAY_CASH_LOAN_PRODUCT = 'Altara Pay Renewal Cash Loan(Product)';
         const ALTARA_PAY_STARTER_CASH_LOAN = 'Altara Pay Starter Cash Loan';
         const ALTARA_PAY_RENTALS = 'Altara Pay Rentals';
         const ALTARA_PAY_EMPLOYEE_CASH_LOAN = 'Altara Pay Employee Cash Loan';
