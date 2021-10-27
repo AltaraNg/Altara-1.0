f821dd590db8dca61546b891fdba712ef4a49a11
harmlessprince <realolamilekan@gmail>
2021-10-27 14:31:29 +0100

    fixed wrong filtering of order for DSA agents and shwoing right creator of renewal prompter

--- app/Http/Filters/NewOrderFilter.php
+++ app/Http/Filters/NewOrderFilter.php
@@ -196,11 +196,11 @@ class NewOrderFilter extends BaseFilter
     }
     public function filterOrderByBranch($filterOrderByBranch=true)
     {
+
         if ($filterOrderByBranch){
             if (auth()->user()->isDSACaptain()) {
                 $this->builder->where('branch_id', auth()->user()->branch_id);
             } else if (auth()->user()->isCoordinator()) {
-
                 // ** Might need refactoring
                 $branches = auth()->user()->branches;
                 $ids = $branches->map(function ($branch) {
@@ -208,7 +208,7 @@ class NewOrderFilter extends BaseFilter
                 });
                 $this->builder->whereIn('branch_id', $ids);
             } else if (auth()->user()->isDSAAgent()) {
-                $this->builder->where('user_id', auth()->user()->id);
+                $this->builder->where('owner_id', auth()->user()->id);
             }
         }
     }
--- app/RenewalPrompter.php
+++ app/RenewalPrompter.php
@@ -21,12 +21,17 @@ class RenewalPrompter extends Model
         return $this->belongsTo(NewOrder::class, 'order_id');
     }
 
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
     public function  allowedAttributes() :\Illuminate\Support\Collection {
         return collect([
             'order_id' => $this->order_id ?? '',
             'renewal_prompter_status' => $this->renewalPrompterStatus->name ?? '',
             'owner' => $this->order->owner->full_name ?? '',
-            'creator' =>  $this->order->user->full_name ?? '',
+            'creator' =>  $this->creator->full_name ?? '',
             'branch' => $this->order->branch->name ?? '',
             'feedback' => $this->feedback ?? '',
             'created_at' => $this->created_at,
