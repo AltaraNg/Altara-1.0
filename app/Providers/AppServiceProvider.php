c003ef58e6a418b2613c6b68ed1bb8ac8aadbf2e
realolamilekan@gmail.com <realolamilekan@gmail.com>
2022-01-25 14:50:42 +0100

    added newly relaesed products

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,6 +18,17 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        //Macro
+        Builder::macro('appplyLikeOnMultipleSearchTerms', function ($attribute, array $searchTerms) {
+            $this->where(function (Builder $query) use ($attribute, $searchTerms) {
+                foreach (Arr::wrap($searchTerms) as $searchTerm) {
+                    $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                }
+            });
+            return $this;
+        });
+
     }
 
     /**
