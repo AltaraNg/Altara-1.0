fe8ff41d2a118e4672a9fd3c2214b3191f9ca9dd
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2022-03-03 13:59:33 +0100

    Paystack customer store update  (#611)
    * Paystack Customer Code Fix
    
    * updated store funtion for paystack code customer
    
    * updated function for store
    
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- app/Http/Controllers/PaystackCustomersCodeController.php
+++ app/Http/Controllers/PaystackCustomersCodeController.php
@@ -4,21 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Helper\ResponseHelper;
 use App\Http\Requests\PaystackCustomersCodeRequest;
+use App\PaystackCustomersCode;
+use App\Repositories\CustomerRepository;
 use App\Repositories\PaystackCustomersCodeRepository;
 
 class PaystackCustomersCodeController extends Controller
 {
     //
-    private $paystackCustomerCode;
+    private $payStackCustomerCodeRepo;
+    private $customerRepository;
 
-    public function __construct(PaystackCustomersCodeRepository $paystackCustomersCodeRepo)
+    public function __construct(PaystackCustomersCodeRepository $payStackCustomerCodeRepo, CustomerRepository $customerRepository)
     {
-        $this->paystackCustomerCode = $paystackCustomersCodeRepo;
+        $this->payStackCustomerCodeRepo = $payStackCustomerCodeRepo;
+        $this->customerRepository = $customerRepository;
     }
 
-    public function store(PaystackCustomersCodeRequest $request)
+    public function store(PaystackCustomersCode $model,PaystackCustomersCodeRequest $request)
     {
-        $result = $this->paystackCustomerCode->store($request->validated());
+        $result = $this->payStackCustomerCodeRepo->updateOrCreate($model, $request->id, $request->validated());
         return ResponseHelper::createSuccessResponse($result->toArray());
     }
 }
