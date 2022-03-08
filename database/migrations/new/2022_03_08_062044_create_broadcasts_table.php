6991852db1f77d89aa16211f86a3f735ffd92912
harmlessprince <realolamilekan@gmail>
2022-03-08 07:37:26 +0100

    added broadcast table

---
+++ database/migrations/new/2022_03_08_062044_create_broadcasts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBroadcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broadcasts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('subject');
+            $table->text('messages');
+            $table->text('data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broadcasts');
+    }
+}
