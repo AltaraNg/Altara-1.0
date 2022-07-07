346ceedf83fb574e42c199564973f9d24cb6f780
Omodamola Oladeji <darmhoo@hotmail>
2022-07-07 08:59:20 +0000

    added new cashloans

--- app/BusinessType.php
+++ app/BusinessType.php
@@ -15,6 +15,11 @@ class BusinessType extends Model
     const ALTARA_PAY_STARTER_CASH_LOAN_SLUG = 'ap_starter_cash_loan';
     const ALTARA_PAY_RENTALS_SLUG = 'ap_rentals';
     const ALTARA_PAY_EMPLOYEE_CASH_LOAN_SLUG = 'ap_employee_cash_loan';
+    const ALTARA_PAY_STARTER_CASH_NINE_MONTHS = 'ap_starter_cash_nine_months';
+    const ALTARA_PAY_SUPER_LOAN_RENEWAL = 'ap_super_loan-renewal';
+    const ALTARA_PAY_SUPER_LOAN_NEW = 'ap_super_loan-new';
+    const ALTARA_PAY_CASH_LOAN_NO_COLLATERAL = 'ap_cash_loan-no_collateral';
+    const ALTARA_PAY_STARTER_CASH_LOAN_NO_COLLATERAL = 'ap_starter_cash_loan-no_collateral';
 
     //
     public function newOrder()
