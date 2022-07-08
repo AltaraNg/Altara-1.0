3ea66bb0ca09bd966ddff3519511f6db9d94a1a0
Omodamola Oladeji <darmhoo@hotmail>
2022-07-08 07:23:25 +0000

    endpoint to log latefee

--- app/Http/Controllers/LateFeeController.php
+++ app/Http/Controllers/LateFeeController.php
@@ -2,15 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\LateFeeDebitEvent;
 use App\Http\Filters\LateFeeFilter;
+use App\LateFee;
+use App\PaymentMethod;
+use App\PaymentType;
 use App\Repositories\LateFeeRepository;
+use App\Services\PaymentService;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class LateFeeController extends Controller
 {
     //
     private $lateFeeRepo;
-    public function __construct(LateFeeRepository $lateFeeRepository){
+    public function __construct(LateFeeRepository $lateFeeRepository)
+    {
         $this->lateFeeRepo = $lateFeeRepository;
     }
 
@@ -19,5 +26,25 @@ class LateFeeController extends Controller
         $lateFees = $this->lateFeeRepo->getAll($filter);
 
         return $this->sendSuccess($lateFees->toArray(), 'Late fees retrieved successfully');
+    }
+
+    public function update(LateFee $lateFee)
+    {
+        $today = Carbon::now()->toDateString();
+        $lateFee->date_paid = $today;
+        $lateFee->save();
+
+        $data_for_log = [
+            "amount" => $lateFee->amount,
+            "customer_id" => $lateFee->new_orders->customer_id,
+            "payment_type_id" => PaymentType::where('type', PaymentType::LATE_FEE)->first()->id,
+            "payment_method_id" => PaymentMethod::where('name', 'direct-debit')->first()->id,
+            "bank_id" => 6 //hardcoded to fcmb
+        ];
+        PaymentService::logPayment($data_for_log, $lateFee->new_orders);
+        event(new LateFeeDebitEvent($lateFee));
+
+
+        return $this->sendSuccess($lateFee->toArray(), 'Late fee updated successfully');
     }
 }
