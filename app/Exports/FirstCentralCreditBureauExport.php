ad946c910cb0a0a501420298884b308561c54867
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2022-08-16 14:09:15 +0100

    Updated First Central Query (#654)
    * first central
    
    * first central
    
    * updated first central
    
    Co-authored-by: harmlessprince <realolamilekan@gmail>

--- app/Exports/FirstCentralCreditBureauExport.php
+++ app/Exports/FirstCentralCreditBureauExport.php
@@ -3,9 +3,10 @@
 namespace App\Exports;
 
 use App\Customer;
-use App\Exports\Sheets\CreditInformationSheet;
+use App\NewOrder;
 use App\Exports\Sheets\CustomerSheet;
 use Maatwebsite\Excel\Concerns\Exportable;
+use App\Exports\Sheets\CreditInformationSheet;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 class FirstCentralCreditBureauExport implements WithMultipleSheets
@@ -13,14 +14,16 @@ class FirstCentralCreditBureauExport implements WithMultipleSheets
     use Exportable;
     private $customersQuery;
     private $newOrdersQuery;
-    public function __construct($customersQuery, $newOrdersQuery)
+    public function __construct()
     {
-        $this->newOrdersQuery = $newOrdersQuery;
-        $this->customersQuery = $customersQuery;
+        $from = '2021-01-01';
+        $to = '2021-12-28';
+        $this->customersQuery = Customer::query()->whereBetween('created_at', [$from, $to])->has('new_orders');
+        $customerIds = (clone $this->customersQuery)->select('id')->get()->pluck('id')->toArray();
+        $this->newOrdersQuery = NewOrder::query()->whereIn('customer_id', $customerIds)->has('amortization')->with(['amortization', 'latestAmortizationNotPayed', 'latestAmortizationPayed', 'customer']);
     }
     public function sheets(): array
     {
-        set_time_limit(100);
         return [
             new CustomerSheet($this->customersQuery),
             new CreditInformationSheet($this->newOrdersQuery),
--- app/Exports/Sheets/CreditInformationSheet.php
+++ app/Exports/Sheets/CreditInformationSheet.php
@@ -2,41 +2,46 @@
 
 namespace App\Exports\Sheets;
 
+use Generator;
+use App\NewOrder;
 use Carbon\Carbon;
-use Maatwebsite\Excel\Concerns\Exportable;
-use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\FromQuery;
-use Maatwebsite\Excel\Concerns\ShouldAutoSize;
-use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithLimit;
-use Maatwebsite\Excel\Concerns\WithMapping;
-use Maatwebsite\Excel\Concerns\WithProperties;
 use Maatwebsite\Excel\Concerns\WithTitle;
+use Maatwebsite\Excel\Concerns\Exportable;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\FromGenerator;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithProperties;
+use Maatwebsite\Excel\Concerns\WithCustomChunkSize;
 
-class CreditInformationSheet implements FromCollection, WithHeadings, WithMapping, WithTitle, ShouldAutoSize
+class CreditInformationSheet implements FromGenerator, WithHeadings, WithMapping, WithTitle, ShouldAutoSize, WithLimit, WithCustomChunkSize
 {
-    use Exportable;
 
     private $orders;
     public function __construct($orders)
     {
         $this->orders = $orders;
-        // dd($orders->limit(10)->count());
     }
     public function title(): string
     {
         return 'Credit Information';
     }
-    public function collection()
+    // public function query()
+    // {
+    //     // dd($this->orders->count());
+    //     return $this->orders;
+    // }
+    public function generator(): Generator
     {
-        return $this->orders->get();
+        // 
+        return $this->orders->cursor();
     }
 
     public function map($order): array
     {
-
-        // dd($this->loanClassification($order));
-        // dd(  $order->amortization[$order->amortization->count() - 1]);
+        //   dd($order);
         $lastNonePaymentDate = null;
         $lastPaymentDate = null;
         $days_in_arrears = '0';
@@ -44,6 +49,7 @@ class CreditInformationSheet implements FromCollection, WithHeadings, WithMappin
             $lastPaymentDate  = $order->latestAmortizationPayed->actual_payment_date;
         }
         if ($order->latestAmortizationNotPayed) {
+            // dd($order->latestAmortizationNotPayed);
             $lastNonePaymentDate = $order->latestAmortizationNotPayed->expected_payment_date;
         }
 
@@ -53,12 +59,17 @@ class CreditInformationSheet implements FromCollection, WithHeadings, WithMappin
 
         $outStandingBalance = $this->outstandingBalance(clone $order) ?? '0';
         $accountStatus = $this->openOrClosed(clone $order);
+        $accountStatusDate = $this->accountStatusDate($accountStatus, $order);
+        $overDueAmount =  $this->overdueAmount(clone $order) ?? '0';
+        $repaymentFrequencyName =  $this->repaymentCycleName($order->repaymentCycle->name);
+        $loanClassification =  $this->loanClassification($days_in_arrears);
+        $maturityDate = $order->amortization[$order->amortization->count() - 1]->expected_payment_date;
 
         return [
             $order->customer->id, // 'Customer ID',
             $order->id, // 'Account Number',
             $accountStatus, // 'Account Status',
-            $this->accountStatusDate($accountStatus, $order), // 'Account status date',
+            $accountStatusDate, // 'Account status date',
             $order->order_date, // 'Date of loan (facility) disbursement/Loan effective date',
             $order->product_price,   // 'Credit limit/Facility amount/Global limit',
             $order->product_price, // 'Loan (Facility) Amount/Availed Limit',
@@ -66,14 +77,14 @@ class CreditInformationSheet implements FromCollection, WithHeadings, WithMappin
             $order->amortization[0]->expected_amount,    // 'Instalment amount',
             'NGN',  // 'Currency',
             $days_in_arrears ?? '0', // 'Days in arrears',
-            $this->overdueAmount(clone $order) ?? '0', // 'Overdue amount',
+            $overDueAmount, // 'Overdue amount',
             'personal', // 'Loan (Facility) type',
             $order->repaymentDuration->value, // 'Loan (Facility) Tenor',
-            $this->repaymentCycleName($order->repaymentCycle->name),   // 'Repayment frequency',
+            $repaymentFrequencyName,   // 'Repayment frequency',
             $order->latestAmortizationPayed->actual_payment_date ?? '',   // 'Last payment date',
             $order->latestAmortizationPayed->actual_amount ?? '', // 'Last payment amount',
-            $order->amortization[$order->amortization->count() - 1]->expected_payment_date, // 'Maturity date',
-            $this->loanClassification($days_in_arrears), // 'Loan Classification', //ASK cat 1
+            $maturityDate, // 'Maturity date',
+            $loanClassification, // 'Loan Classification', //ASK cat 1
             $order->customer->civil_status, // 'Marital Status',
             'N/A', // 'Spouse\'s Name',
             'N/A', // 'Legal Challenge Status ',
@@ -142,19 +153,27 @@ class CreditInformationSheet implements FromCollection, WithHeadings, WithMappin
 
     public function openOrClosed($order)
     {
-        $sumOfRepaymentMade = (clone $order)->amortization->where('actual_payment_date', '!=', null)->sum('actual_amount');
-       
-        $expectedSumOfPayment = $order->amortization->where('actual_payment_date', '=', null)->sum('expected_amount');     
-        return $expectedSumOfPayment <= $sumOfRepaymentMade ? 'Closed' : 'Open';
+        $sumOfRepaymentMade = (clone $order)->amortization->where('actual_payment_date', '<>', null)->where('actual_amount', '>', 1)->sum('actual_amount');
+
+        $expectedSumOfPayment = $order->amortization->sum('expected_amount');
+
+        $amountLeftToBePayed =  $expectedSumOfPayment - $sumOfRepaymentMade;
+        if ($amountLeftToBePayed > 0) {
+            return 'Open';
+        } elseif ($amountLeftToBePayed == 0) {
+            return 'Closed';
+        } else {
+            return 'Closed';
+        }
     }
 
     public function accountStatusDate($status, $order)
     {
         $date = null;
-        if ($status == 'Closed') {
+        if ($status == 'Closed' && $order->latestAmortizationPayed) {
             $date =  $order->latestAmortizationPayed->actual_payment_date;
         }
-        if ($status == 'Open') {
+        if ($status == 'Open' &&  $order->latestAmortizationNotPayed) {
             $date = $order->latestAmortizationNotPayed->expected_payment_date;
         }
         return $date;
@@ -185,5 +204,14 @@ class CreditInformationSheet implements FromCollection, WithHeadings, WithMappin
             return 'Monthly';
         }
         return 'N/A';
+    }
+    public function limit(): int
+    {
+        return 500;
+    }
+
+    public function chunkSize(): int
+    {
+        return 500;
     }
 }
