30f6c93330698091929631778d7cebcb82c20842
Omodamola Oladeji <darmhoo@hotmail>
2022-10-14 14:12:33 +0000

    created gearantor table

---
+++ database/migrations/new/2022_10_13_210045_create_guarantor_paystack_auth_codes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGuarantorPaystackAuthCodesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guarantor_paystack_auth_codes', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('customer_id');
+            $table->string('guarantor_email');
+            $table->string('guarantor_name');
+            $table->string('auth_code');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guarantor_paystack_auth_codes');
+    }
+}
