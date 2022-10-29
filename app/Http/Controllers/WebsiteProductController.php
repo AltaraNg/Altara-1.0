b061be0c666d6398467980d8628ab7daa0d19631
Omodamola Oladeji <darmhoo@hotmail.com>
2022-10-29 14:40:30 +0000

    created migration file

---
+++ app/Http/Controllers/WebsiteProductController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\WebsiteProduct;
+use Illuminate\Http\Request;
+
+class WebsiteProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\WebsiteProduct  $websiteProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(WebsiteProduct $websiteProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\WebsiteProduct  $websiteProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(WebsiteProduct $websiteProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\WebsiteProduct  $websiteProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, WebsiteProduct $websiteProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\WebsiteProduct  $websiteProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(WebsiteProduct $websiteProduct)
+    {
+        //
+    }
+}
