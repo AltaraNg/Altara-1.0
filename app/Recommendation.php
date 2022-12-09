1e21fdc9e9f18d28102dedb1e276b2166581b675
Omodamola Oladeji <darmhoo@hotmail.com>
2022-12-09 12:05:23 +0000

    created endpoint for fetching recommendation

--- app/Recommendation.php
+++ app/Recommendation.php
@@ -8,4 +8,8 @@ class Recommendation extends Model
 {
     //
     protected $guarded = [];
+
+    public function customer(){
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
 }
