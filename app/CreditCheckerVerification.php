52d99c53fca89eb20840dafe9b3c023ffa7204f7
harmlessprince <realolamilekan@gmail>
2023-02-16 16:45:34 +0100

    added commssion to new orders

--- app/CreditCheckerVerification.php
+++ app/CreditCheckerVerification.php
@@ -9,7 +9,7 @@ class CreditCheckerVerification extends Model
 
     const PENDING = 'pending';
     const PASSED = 'passed';
-    const FAILED = 'FAILED';
+    const FAILED = 'failed';
     const STATUSES = [self::PENDING, self::PASSED, self::FAILED];
 
     protected $guarded = [];
