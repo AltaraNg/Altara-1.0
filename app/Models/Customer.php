188bd3ccd4930753bce632832d7fecbd347c942f
Oladeji Omodamola <darmhoo@hotmail.com>
2023-07-12 10:55:44 +0000

    fix unallowed access

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -237,7 +237,8 @@ class Customer extends Model
     protected function bvn(): Attribute
     {
         return Attribute::make(
-            get: fn($value) => in_array(auth()->user()->role_id, User::BVN_ACCESS) ? $value : -1,
+            get: fn($value) => $value === null ? $value : (in_array(auth()->user()->role_id, User::BVN_ACCESS) ? $value : -1),
+
         );
     }
 
