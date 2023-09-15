e5dd58d9f0488aacd16a39c18f6bfec2e40e4ec8
harmlessprince <realolamilekan@gmail>
2023-09-15 08:11:59 +0100

    refactored get attribute ob customer model and set bvn first central

--- app/Exports/Sheets/CustomerSheet.php
+++ app/Exports/Sheets/CustomerSheet.php
@@ -50,7 +50,7 @@ class CustomerSheet implements FromCollection, WithHeadings, WithMapping, WithTi
             $dateOfBirth,   // 'Date of Birth',
             'N/A', // 'National Identity Number',
             'N/A', // 'Drivers License No',
-            $customer->bvn_no ?? 'N/A', // 'BVN No',
+            $customer->bvn ?? 'N/A', // 'BVN No',
             $customer->passport_no ?? 'N/A', // 'Passport No',
             $customer->gender, // 'Gender',
             'NG',   // 'Nationality',
