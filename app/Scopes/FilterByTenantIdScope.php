e2234c5bde453c0d569c0d7ebc05ad91e54a51b4
harmlessprince <realolamilekan@gmail>
2023-10-03 15:56:30 +0100

    Added Scope To filter by tenant id

---
+++ app/Scopes/FilterByTenantIdScope.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Scopes;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
+
+class FilterByTenantIdScope implements Scope
+{
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        /** @var User $user */
+        if (Auth::hasUser()) {
+            $user = auth()->user();
+            if (!UserService::hasAnyRole($user, [1,2,8,9])) {
+                return $builder->where('tenant_id', $user->tenant_id);
+            }
+        }
+    }
+}
