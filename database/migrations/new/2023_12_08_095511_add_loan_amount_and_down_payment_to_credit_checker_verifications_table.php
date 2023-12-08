58f69c3c2ff68dc0ad879265e6331409bb52fb1b
Adewuyi Taofeeq Olamilekan <36686152+harmlessprince@users.noreply.github.com>
2023-12-08 10:11:52 +0100

    Loan Amount and Download (#875)
    * mobile app audit trail
    
    * added extra coloumn on credit checkk table
    
    * loading product
    
    * refactoring
    
    * sending notifications out
    
    * order can now be previewed and logged
    
    * Customer loan approval
    
    * adde check to make sure credit check is valid
    
    * added is admin coloumn to mobile_app_activities
    
    * event
    
    * added event for listening to mobile app events
    
    * added event for listening to mobile app events
    
    * added loging for repayment from portal
    
    * added audit for when order is completed
    
    * fixed downpayment event issue
    
    * added an extra credit check status
    
    * added decimal to verification
    
    * added repayment to verification table
    
    * done
    
    * fixed
    
    * added loan amount and down_payment
    
    ---------
    
    Co-authored-by: harmlessprince <realolamilekan@gmail>

---
+++ database/migrations/new/2023_12_08_095511_add_loan_amount_and_down_payment_to_credit_checker_verifications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('credit_checker_verifications', function (Blueprint $table) {
+            $table->decimal('loan_amount', 10, 2)->nullable();
+            $table->decimal('down_payment', 10, 2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('credit_checker_verifications', function (Blueprint $table) {
+            $table->dropColumn('loan_amount');
+            $table->dropColumn('down_payment');
+        });
+    }
+};
